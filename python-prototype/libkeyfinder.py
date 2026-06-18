4240479554d5724538b5de064d36653cef6cf123
duvkorp <duvkorp@gmail.com>
2026-06-18 15:51:47 +0800

    replace audio with pure sine wave

--- python-prototype/libkeyfinder.py
+++ python-prototype/libkeyfinder.py
@@ -5,15 +5,19 @@ import numpy as np
 import approximate_cqt as cqt
 import matplotlib.pyplot as plt
 
-# audio_io
-sample_rate, audio_data = wavfile.read("audio_io.wav")
-
-if(len(audio_data.shape) > 1):
-    audio_data = audio_data[:, 0]
-
 # param
+# fft
+sample_rate = 11025
 framesize = 4096
 hopsize = framesize / 4
+
+# cqt
+bands = 72
+offsets, kernels = cqt.build_kernal(bands, framesize, sample_rate)
+
+# A440 sin wave
+t = np.linspace(0, 2.0, int(sample_rate * 2.0), endpoint=False)
+audio_data = np.sin(2 * np.pi * 440 * t)
 
 # pick a frame
 start = 3 * framesize
@@ -24,15 +28,11 @@ frame = audio_data[start:end] * hamming
 # fft
 mag = np.abs( fft.rfft(frame) )
 
-# param
-bands = 72
-
-# lookup table
-offsets, kernels = cqt.build_kernal(bands, framesize, sample_rate)
-
 # approximate cqt
 cqt_72 = cqt.apply_kernel(mag, offsets, kernels)
 
 # fold to chroma
-cqt_72.reshape(6, 12).sum(axis=0)
+chroma = cqt_72.reshape(6, 12).sum(axis=0)
+print(chroma)
+
 
