c73c2cd746c85969161e6f53d4f5bfe13676d563
duvkorp <duvkorp@gmail.com>
2026-06-18 15:18:04 +0800

    create approximate cqt api

---
+++ python-prototype/approximate_cqt.py
@@ -0,0 +1,47 @@
+import numpy as np
+
+def band_freq(band_index):
+    # A1(hz) * 2 ^ (i / semitones)
+    return 55.0 * (2 ** (band_index / 12)) 
+
+def build_kernal(bands, framesize, sample_rate):
+    Q = 1 / (2 ** (1.0 / 12) - 1) 
+    
+    """
+    band 0  (A1, 55 Hz):
+    offsets[0] = 5          ← "start at FFT bin 5"
+    kernels[0] = [0.3, 0.8, 1.2, 0.8, 0.3]   ← 5 weights, one per bin
+    """
+
+    offsets = []
+    kernels = []
+
+    for i in range(bands):
+        center_freq = band_freq(i)
+        center_bin = center_freq * framesize / sample_rate
+        bin_width = center_freq / Q
+
+        begin = np.ceil(center_bin - bin_width/2)
+        end = np.floor(center_bin + bin_width/2)
+        
+        bins_in_window = np.arage(begin, end+1)
+
+        n = bins_in_window - begin
+        coeffs = 1.0 - np.cos(2 * np.pi * n / bin_width)
+
+        coeffs = coeffs / coeffs.sum() * center_freq
+
+        offsets.append(int(begin))
+        kernels.append(coeffs)
+
+    return offsets, kernels
+
+def apply_kernel(mag, offsets, kernels):
+
+    cqt = np.zeros(len(kernels)) # 72 bands
+
+    for i, (offset, coeffs) in enumerate(zip(offsets, kernels)):
+        bins = mag[offset : offset + len(coeffs)]
+        cqt[i] = np.dot(bins, coeffs)
+
+    return cqt
