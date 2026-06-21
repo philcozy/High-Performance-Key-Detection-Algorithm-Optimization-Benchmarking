6e5abedb8275dbdd98c71d289d9039d5ce0b4272
duvkorp <duvkorp@gmail.com>
2026-06-21 16:19:58 +0800

    fix keyprofile bug

--- python-prototype/keyprofile.py
+++ python-prototype/keyprofile.py
@@ -6,14 +6,16 @@ nbb_major = [ 0.0804, 0.0062, 0.1057, 0.1680, 0.0086, 0.1295,
 nbb_minor = [ 0.0153, 0.0092, 0.1021, 0.1816, 0.0069, 0.1299, 
               0.1334, 0.0107, 0.1115, 0.0138, 0.2107, 0.0749, ]
 
-states = ('C', 'Db', 'D', 'Eb', 'E', 'F', 'F#', 'G', 'Ab', 'A', 'Bb', 'B',
-          'c', 'c#', 'd', 'eb', 'e', 'f', 'f#', 'g', 'ab', 'a', 'bb', 'b' )
+states = ('A', 'Bb', 'B', 'C', 'Db', 'D', 'Eb', 'E', 'F', 'F#', 'G', 'Ab'
+          'a', 'bb', 'b', 'c', 'c#', 'd', 'eb', 'e', 'f', 'f#', 'g', 'ab')
 tone = ("major", "minor")
 
 def get_profile_for_key(idx, major=nbb_major, minor=nbb_minor):
     rotation = -(idx % 12)
-    if idx < 12: return (major[rotation:] + major[:rotation])
-    else: return (minor[rotation:] + minor[:rotation])
+    if idx < 12: 
+        return (major[rotation:] + major[:rotation])
+    else: 
+        return (minor[rotation:] + minor[:rotation])
 
 def get_key(chroma):
     best_score = -1
